98f32317863d851ba59e146acf2e5ccaee30b24b
aminelebrini <aminelebrini4@gmail.com>
2024-02-26 14:48:59 +0100

    strlength

---
+++ strlength.java
@@ -0,0 +1,13 @@
+public class strlength{
+	public static void main(String[] args)
+	{
+		String s = "audi";
+		int i = 0;
+		
+		while(i < s.length())
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+}
